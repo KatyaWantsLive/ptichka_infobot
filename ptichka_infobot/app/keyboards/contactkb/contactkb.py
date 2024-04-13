c80a82496340d51e4a21897c71d97a0d0c8810b7
svetsss <92213711+svetsss@users.noreply.github.com>
2024-04-13 10:04:27 +0500

    Update contactkb.py

--- ptichka_infobot/app/keyboards/contactkb/contactkb.py
+++ ptichka_infobot/app/keyboards/contactkb/contactkb.py
@@ -2,8 +2,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 contacts = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text = 'Остались вопросы?', callback_data='questions'),
-     InlineKeyboardButton(text = 'Хотите сотрудничать?', url='https://inlnk.ru/YAzPA0')],
-     [InlineKeyboardButton(text='На глвную', callback_data='go_home')]
+     InlineKeyboardButton(text = 'Сотрудничество', url='https://inlnk.ru/YAzPA0')],
+     [InlineKeyboardButton(text='На главную', callback_data='go_home')]
 ])
 
 back = InlineKeyboardMarkup(inline_keyboard=[

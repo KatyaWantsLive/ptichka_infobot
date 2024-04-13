2283fc595d3d962fa84e2c72d1fa6cb5d80437d2
svetsss <92213711+svetsss@users.noreply.github.com>
2024-04-13 10:04:01 +0500

    Update ruleskb.py

--- ptichka_infobot/app/keyboards/ruleskb/ruleskb.py
+++ ptichka_infobot/app/keyboards/ruleskb/ruleskb.py
@@ -1,9 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 rules = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text = 'что можно сдать в пункт?', callback_data='what'),
-     InlineKeyboardButton(text = 'как правильно сдавать?', callback_data='how')],
-     [InlineKeyboardButton(text='На глвную', callback_data='go_home')]
+    [InlineKeyboardButton(text = 'Типы вторсырья', callback_data='what'),
+     InlineKeyboardButton(text = 'Правила сдачи', callback_data='how')],
+     [InlineKeyboardButton(text='На главную', callback_data='go_home')]
 ])
 
 back = InlineKeyboardMarkup(inline_keyboard=[
